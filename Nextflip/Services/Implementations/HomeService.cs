3656c14aac272f9a23a48a12cc62eac0b021803a
ducthethird <ducttse@gmail.com>
2021-06-01 23:02:28 +0700

    add mock service

---
+++ Nextflip/Services/Implementations/HomeService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Nextflip.Services.Interfaces;
+
+namespace Nextflip.Services.Implementations
+{
+    public class HomeService : IHomeService
+    {
+    }
+}

81eaaf0a504f04a994a776123c36c5f63a98f7ed
ducthethird <ducttse@gmail.com>
2021-06-01 08:20:22 +0700

    Add project files.

---
+++ Nextflip/APIController/Home.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nextflip.APIController
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Home : ControllerBase
+    {
+        public ActionResult Get()
+        {
+            return new JsonResult("abc");
+        }
+    }
+}
---
+++ Nextflip/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Nextflip.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

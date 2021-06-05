72dd3e0fa61e6f6722afab825d4f2f7001c2629f
ducthethird <ducttse@gmail.com>
2021-06-05 13:20:43 +0700

    Layout bug

--- Nextflip/Startup.cs
+++ Nextflip/Startup.cs
@@ -31,8 +31,8 @@ namespace Nextflip
         {
             services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+            //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            //    .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
 
             ///get connection string

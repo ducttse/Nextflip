53a5bc2f023ed9c6a64bafa68e5159cb453348aa
NADMINB <changeguy94@gmail.com>
2021-06-06 19:44:47 +0700

    6th

--- Nextflip/Startup.cs
+++ Nextflip/Startup.cs
@@ -16,6 +16,14 @@ using System.Threading.Tasks;
 using Nextflip.utils;
 using Nextflip.Services.Interfaces;
 using Nextflip.Services.Implementations;
+using Nextflip.Models.category;
+using Nextflip.Models.episode;
+using Nextflip.Models.favoriteList;
+using Nextflip.Models.media;
+using Nextflip.Models.mediaCategory;
+using Nextflip.Models.mediaFavorite;
+using Nextflip.Models.season;
+using Nextflip.Models.subtitle;
 
 namespace Nextflip
 {
@@ -33,11 +41,14 @@ namespace Nextflip
         {
             services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddTransient<ICategoryService,CategoryService>();
-            services.AddTransient<IEpisodeService, EpisodeService>();
-            services.AddTransient<IMediaService, MediaService>();
-            services.AddTransient<ISeasonService, SeasonService>();
-            services.AddTransient<ISubtitleService, SubtitleService>();
+            services.AddTransient<ICategoryDAO, CategoryDAO>();
+            services.AddTransient<IEpisodeDAO, EpisodeDAO>();
+            services.AddTransient<IMediaDAO, MediaDAO>();
+            services.AddTransient<IFavoriteListDAO, FavoriteListDAO>();
+            services.AddTransient<IMediaCategoryDAO, MediaCategoryDAO>();
+            services.AddTransient<IMediaFavoriteDAO, MediaFavoriteDAO>();
+            services.AddTransient<ISeasonDAO, SeasonDAO>();
+            services.AddTransient<ISubtitleDAO, SubtitleDAO>();
             //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
             //    .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();

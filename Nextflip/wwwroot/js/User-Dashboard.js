3435571bb1ee72bbc5b0c9df567d6edd763cb375
Long <tranbaolong14@gmail.com>
2021-06-07 17:12:23 +0700

    add view

---
+++ Nextflip/wwwroot/js/User-Dashboard.js
@@ -0,0 +1,61 @@
+let Data = {
+  data: [
+    {
+      mediaID: "M001",
+      title: "Spirder-Man",
+      status: "Enabled",
+      language: "English",
+      bannerURL: "https://storage.googleapis.com/next-flip/Image/Banner"
+    },
+    {
+      mediaID: "M001",
+      title: "Spirder-Man",
+      status: "Enabled",
+      language: "English",
+      bannerURL: "https://storage.googleapis.com/next-flip/Image/Banner"
+    },
+    {
+      mediaID: "M001",
+      title: "Spirder-Man",
+      status: "Enabled",
+      language: "English",
+      bannerURL: "https://storage.googleapis.com/next-flip/Image/Banner"
+    },
+    {
+      mediaID: "M001",
+      title: "Spirder-Man",
+      status: "Enabled",
+      language: "English",
+      bannerURL: "https://storage.googleapis.com/next-flip/Image/Banner"
+    }
+  ]
+};
+
+function renderMedia(media) {
+  return `
+  <div class="col-3 w-25" id="mediaWapper">
+    <img
+      src="${media.bannerURL}"
+      alt="img"
+      class="w-100 h-100"
+    />
+    <p class="bg-dark text-white title">${media.title}</p>
+  </div>`;
+}
+
+function appendMedia(start, end) {
+  let mediaArray = Data.data.slice(start, end).map((media) => {
+    return renderMedia(media);
+  });
+  mediaArray = mediaArray.join("");
+  document
+    .getElementById("wapper")
+    .insertAdjacentHTML("afterbegin", mediaArray);
+}
+
+fetch("/api/ViewSubscribedUserDashboard/GetMediasByCategoryID/1")
+  .then((res) => res.json())
+  .then((json) => {
+    Data.data = json;
+    appendMedia(0, 4);
+  });

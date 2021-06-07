1353b20afc6e9fee6120727e5c4d0b4bbaccc704
Long <tranbaolong14@gmail.com>
2021-06-07 22:46:59 +0700

    pushing

--- Nextflip/wwwroot/js/User-Dashboard.js
+++ Nextflip/wwwroot/js/User-Dashboard.js
@@ -47,10 +47,9 @@ function appendMedia(start, end) {
   let mediaArray = Data.data.slice(start, end).map((media) => {
     return renderMedia(media);
   });
-  mediaArray = mediaArray.join("");
-  document
-    .getElementById("wapper")
-    .insertAdjacentHTML("afterbegin", mediaArray);
+    mediaArray = mediaArray.join("");
+    document.getElementById("wrapper1").insertAdjacentHTML("afterbegin", mediaArray);
+    document.getElementById("wrapper2").insertAdjacentHTML("afterbegin", mediaArray);
 }
 
 fetch("/api/ViewSubscribedUserDashboard/GetMediasByCategoryID/1")

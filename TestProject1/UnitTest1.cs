4cfceb36e4a64037e517b663aafe1f4a78f703fe
datlt2001 <thanhdat3001@gmail.com>
2021-06-10 23:03:56 +0700

    test

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -6,17 +6,5 @@ namespace TestProject1
 {
     public class UnitTest1
     {
-        public UnitTest1(ITestOutputHelper output)
-        {
-            output.WriteLine(DateTime.Now.ToString());
-        }
-        [Theory]
-        [InlineData("abcd","aba", true)]
-        public void GetAccountById_ExpectRightResult_WithoutException(string title, string content, bool expectedResult)
-        {
-            bool actualResult = new Nextflip.Models.notification.NotificationDAO().AddNotification(title, content);
-            ///assert
-            Assert.Equal(expectedResult, actualResult);
-        }
     }
 }

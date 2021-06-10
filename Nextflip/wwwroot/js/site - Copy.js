1e52b6abcf7fe0b0848a6f221144186ca4c4a705
Long <tranbaolong14@gmail.com>
2021-06-10 22:16:40 +0700

    add view

---
+++ Nextflip/wwwroot/js/site - Copy.js
@@ -0,0 +1,23 @@
+﻿function renderOption(item, index) {
+  return `
+    <option value="${index}">${item.topicName}</option>
+    `;
+}
+
+function appendToWrapper(items) {
+  document.getElementById("option").insertAdjacentHTML("afterbegin", items);
+}
+
+function onLoad(data) {
+  let optionArr = data.map((item, index) => {
+    return renderOption(item, index);
+  });
+  optionArr = optionArr.join("");
+  appendToWrapper(optionArr);
+}
+
+fetch("https://localhost:44341/api/SendSupportTicket/GetAllTopic")
+  .then((res) => res.json())
+  .then((json) => {
+    onLoad(json);
+  });

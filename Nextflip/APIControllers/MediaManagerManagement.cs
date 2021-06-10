79ff14795f5c523cd88c30982933db7d48393e46
datlt2001 <thanhdat3001@gmail.com>
2021-06-10 22:57:27 +0700

    Fix Route

--- Nextflip/APIControllers/MediaManagerManagement.cs
+++ Nextflip/APIControllers/MediaManagerManagement.cs
@@ -9,9 +9,11 @@ using System.Threading.Tasks;
 
 namespace Nextflip.APIControllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class MediaManagerManagement : ControllerBase
     {
-        [Route("api/MediaManagerManagement/")]
+        [Route("GetAllPendingMedias")]
         public JsonResult GetAllPendingMedias([FromServices] IMediaManagerManagementService mediaManagerManagementService)
         {
             IEnumerable<MediaEditRequest> requests = mediaManagerManagementService.GetAllPendingMedias();
@@ -19,6 +21,7 @@ namespace Nextflip.APIControllers
             return new JsonResult(requests);
         }
 
+        [Route("GetPendingMediaByUserEmail/{searchValue}")]
         public JsonResult GetPendingMediaByUserEmail([FromServices] IMediaManagerManagementService mediaManagerManagementService, [FromBody] string searchValue)
         {
             IEnumerable<MediaEditRequest> requests = mediaManagerManagementService.GetPendingMediaByUserEmail(searchValue);
@@ -26,12 +29,15 @@ namespace Nextflip.APIControllers
             return new JsonResult(requests);
         }
 
+        [Route("ApproveRequest/{requestID}")]
         public JsonResult ApproveRequest([FromServices] IMediaManagerManagementService mediaManagerManagementService, [FromBody] int requestID)
         {
             bool result = mediaManagerManagementService.ApproveRequest(requestID);
             Debug.WriteLine("FLAG");
             return new JsonResult(result);
         }
+
+        [Route("DisapproveRequest/{requestID}")]
         public JsonResult DisapproveRequest([FromServices] IMediaManagerManagementService mediaManagerManagementService, [FromBody] int requestID)
         {
             bool result = mediaManagerManagementService.DisappoveRequest(requestID);

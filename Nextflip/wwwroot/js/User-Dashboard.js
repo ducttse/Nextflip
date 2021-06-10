db4639eecbf22383f8671ed27f2df9d94a20e5dd
Long <tranbaolong14@gmail.com>
2021-06-10 21:16:30 +0700

    add user dashboard view

--- Nextflip/wwwroot/js/User-Dashboard.js
+++ Nextflip/wwwroot/js/User-Dashboard.js
@@ -1,6 +1,3 @@
-let Data = {
-  data: []
-};
 let noImage = "https://storage.googleapis.com/next-flip/Image/no%20image.png";
 
 function validateUrl(value) {
@@ -9,25 +6,186 @@ function validateUrl(value) {
   );
 }
 
-function ChangeURL(mediaArray) {
-  let arr = document.getElementById("holder_1").querySelectorAll("img");
-  console.log(arr);
-  for (let i = 0; i < arr.length; i++) {
-    var url = mediaArray[i].bannerURL;
-    if (!validateUrl(url)) {
-      url = noImage;
-    }
-    arr[i].setAttribute("src", url);
+function renderImageHolder(url) {
+  let CheckedURL = noImage;
+  if (validateUrl(url)) {
+    CheckedURL = url;
   }
+  return `
+  <div class="col-3 imageHolder px-1">
+    <img src="${CheckedURL}" class="w-100 h-90" alt="..."/>
+  </div>`;
+}
+
+function renderCarousel(mediaArr, index) {
+  console.log(mediaArr);
+  let renderedImgHolder = mediaArr.map((media) => {
+    return renderImageHolder(media.bannerURL);
+  });
+  renderedImgHolder = renderedImgHolder.join("");
+  return `
+  <div class="carousel-item ${index === 0 ? "active" : ""}"> 
+    <div class="row">
+      ${renderedImgHolder}
+    </div>
+  </div>
+  `;
+}
+
+function renderLisrHolder(listMedia) {
+  let mediaArr = listMedia.mediaArr;
+  let length = Math.floor(mediaArr.length / 4);
+  let renderedCarousels = "";
+  for (let i = 0; i < length; i++) {
+    var subArr = mediaArr.slice(i * 4, (i + 1) * 4);
+    renderedCarousels += renderCarousel(subArr, i);
+  }
+  let button = `
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  `;
+  return `
+  <div class="mt-5 listHolder">
+    <p class="h3 listTitle">${listMedia.name}</p>
+    <div>
+      <div class="carousel slide h-25" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          ${renderedCarousels}
+        </div>
+        ${length <= 1 ? "" : button}
+      </div>
+    </div>
+  </div>`;
+}
+
+function appendToWrapper(renderedEL) {
+  document
+    .getElementById("wrapper")
+    .insertAdjacentHTML("beforeend", renderedEL);
+}
+
+function fetchCategoryID(category) {
+  fetch(
+    `/api/ViewSubscribedUserDashboard/GetMediasByCategoryID/${category.categoryID}`
+  )
+    .then((res) => res.json())
+    .then((json) => {
+      if (json.length === 0) {
+        return;
+      }
+      category.mediaArr = json;
+      appendToWrapper(renderLisrHolder(category));
+    });
+}
+
+function El() {
+  return `
+  <div class="mt-5 listHolder" id="holder_1">
+      <p class="h3 listTitle">Favourite media</p>
+      <div>
+        <div class="carousel slide h-25" data-bs-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active"> 
+              <div class="row">
+                <div class="col-3 imageHolder px-1">
+                  <a href="/WatchMedia/MediaDetails/knoZvTFPyBjmZpzekmOI">
+                    <img
+                      src="https://storage.googleapis.com/next-flip/Image/koe8imWOkoi1dAsd03ds_banner1"
+                      class="w-100 h-90"
+                      alt="..."/>
+                  </a>
+                    </div>
+                <div class="col-3 imageHolder px-1">
+                  <a href="/WatchMedia/MediaDetails/knoZvTFPyBjmZpzekmOI">
+                    <img
+                      src="https://storage.googleapis.com/next-flip/Image/koe8imWOkoi1dAsd03ds_banner1"
+                      class="w-100 h-90"
+                      alt="..."/>
+                  </a>
+                    </div>
+                <div class="col-3 imageHolder px-1">
+                  <a href="/WatchMedia/MediaDetails/knoZvTFPyBjmZpzekmOI">
+                    <img
+                      src="https://storage.googleapis.com/next-flip/Image/koe8imWOkoi1dAsd03ds_banner1"
+                      class="w-100 h-90"
+                      alt="..."/>
+                  </a>
+                    </div>
+                <div class="col-3 imageHolder px-1">
+                  <a href="/WatchMedia/MediaDetails/knoZvTFPyBjmZpzekmOI">
+                    <img
+                      src="https://storage.googleapis.com/next-flip/Image/koe8imWOkoi1dAsd03ds_banner1"
+                      class="w-100 h-90"
+                      alt="..."/>
+                  </a>
+                    </div>
+              </div>
+            </div>
+            <div class="carousel-item"> 
+              <div class="row">
+                <div class="col-3 imageHolder px-1">
+                  <a href="/WatchMedia/MediaDetails/knoZvTFPyBjmZpzekmOI">
+                    <img
+                      src="https://storage.googleapis.com/next-flip/Image/koe8imWOkoi1dAsd03ds_banner1"
+                      class="w-100 h-90"
+                      alt="..."/>
+                  </a>
+                    </div>
+                <div class="col-3 imageHolder px-1">
+                  <a href="/WatchMedia/MediaDetails/knoZvTFPyBjmZpzekmOI">
+                    <img
+                      src="https://storage.googleapis.com/next-flip/Image/koe8imWOkoi1dAsd03ds_banner1"
+                      class="w-100 h-90"
+                      alt="..."/>
+                  </a>
+                    </div>
+                <div class="col-3 imageHolder px-1">
+                  <a href="/WatchMedia/MediaDetails/knoZvTFPyBjmZpzekmOI">
+                    <img
+                      src="https://storage.googleapis.com/next-flip/Image/koe8imWOkoi1dAsd03ds_banner1"
+                      class="w-100 h-90"
+                      alt="..."/>
+                  </a>
+                    </div>
+                <div class="col-3 imageHolder px-1">
+                  <a href="/WatchMedia/MediaDetails/knoZvTFPyBjmZpzekmOI">
+                    <img
+                      src="https://storage.googleapis.com/next-flip/Image/koe8imWOkoi1dAsd03ds_banner1"
+                      class="w-100 h-90"
+                      alt="..."/>
+                  </a>
+                    </div>
+              </div>
+            </div>
+          </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+        </div>
+      </div>
+    </div>`;
 }
 
 function Run() {
-  fetch("/api/ViewSubscribedUserDashboard/GetMediasByCategoryID/1")
+  document.getElementById("wrapper").insertAdjacentHTML("beforeend", El());
+  fetch("/api/ViewSubscribedUserDashboard/GetCategories")
     .then((res) => res.json())
-    .then((json) => {
-      console.log(json);
-      Data.data = json;
-      ChangeURL(Data.data);
+    .then((categories) => {
+      categories.forEach((category) => {
+        fetchCategoryID(category);
+      });
     });
 }
+
 Run();

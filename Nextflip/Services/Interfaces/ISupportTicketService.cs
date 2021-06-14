065408176247adfa6075d58ac3122435f062c1e3
longpnh21 <longpnh2001@gmail.com>
2021-06-14 22:49:33 +0700

    Fix Pagination Issues

--- Nextflip/Services/Interfaces/ISupportTicketService.cs
+++ Nextflip/Services/Interfaces/ISupportTicketService.cs
@@ -10,7 +10,8 @@ namespace Nextflip.Services.Interfaces
     public interface ISupportTicketService
     {
         public bool SendSupportTicket(string userEmail, string topicName, string content);
-        public IList<SupportTicket> ViewAllPendingSupportTickets();
+        public IList<SupportTicket> ViewPendingSupportTickets(int limit, int offset);
         public SupportTicket ViewSupportTicketByID(string supportTicketID);
+        public int GetNumOfSupportTickets();
     }
 }

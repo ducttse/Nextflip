6e7029c4b0d4625f646c560430511578fe3fb2f5
datlt2001 <thanhdat3001@gmail.com>
2021-06-16 18:21:13 +0700

    Write Log

--- Nextflip/APIControllers/MediaManagerManagement.cs
+++ Nextflip/APIControllers/MediaManagerManagement.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Nextflip.Models.mediaEditRequest;
 using Nextflip.Services.Interfaces;
 using System;
@@ -13,44 +14,89 @@ namespace Nextflip.APIControllers
     [ApiController]
     public class MediaManagerManagement : ControllerBase
     {
+        private readonly ILogger _logger;
+
+        public MediaManagerManagement(ILogger<MediaManagerManagement> logger)
+        {
+            _logger = logger;
+        }
+
         [Route("GetAllPendingMedias")]
         public JsonResult GetAllPendingMedias([FromServices] IMediaManagerManagementService mediaManagerManagementService)
         {
+            try
+            {
             IEnumerable<MediaEditRequest> requests = mediaManagerManagementService.GetAllPendingMedias();
-            Debug.WriteLine("FLAG");
             return new JsonResult(requests);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation("GetAllPendingMedias: " + ex.Message);
+                return new JsonResult("Error occur");
+            }
         }
 
         [Route("GetPendingMediaByUserEmail/{searchValue}")]
         public JsonResult GetPendingMediaByUserEmail([FromServices] IMediaManagerManagementService mediaManagerManagementService, [FromBody] string searchValue)
         {
-            IEnumerable<MediaEditRequest> requests = mediaManagerManagementService.GetPendingMediaByUserEmail(searchValue);
-            Debug.WriteLine("FLAG");
-            return new JsonResult(requests);
+            try
+            {
+                IEnumerable<MediaEditRequest> requests = mediaManagerManagementService.GetPendingMediaByUserEmail(searchValue);
+                return new JsonResult(requests);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation("GetPendingMediaByUserEmail: " + ex.Message);
+                return new JsonResult("Error occur");
+            }
         }
 
         [Route("ApproveRequest/{requestID}")]
         public JsonResult ApproveRequest([FromServices] IMediaManagerManagementService mediaManagerManagementService, [FromBody] int requestID)
         {
-            bool result = mediaManagerManagementService.ApproveRequest(requestID);
-            Debug.WriteLine("FLAG");
-            return new JsonResult(result);
+            try
+            {
+                bool result = mediaManagerManagementService.ApproveRequest(requestID);
+                return new JsonResult(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation("ApproveRequest: " + ex.Message);
+                return new JsonResult("Error occur");
+            }
+
         }
 
         [Route("DisapproveRequest/{requestID}")]
         public JsonResult DisapproveRequest([FromServices] IMediaManagerManagementService mediaManagerManagementService, [FromBody] int requestID)
         {
-            bool result = mediaManagerManagementService.DisappoveRequest(requestID);
-            Debug.WriteLine("FLAG");
-            return new JsonResult(result);
+            try
+            {
+                bool result = mediaManagerManagementService.DisappoveRequest(requestID);
+                return new JsonResult(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation("DisapproveRequest: " + ex.Message);
+                return new JsonResult("Error occur");
+            }
         }
 
         [Route("NumberOfPendingMedias")]
         public JsonResult NumberOfPendingMedias([FromServices] IMediaManagerManagementService mediaManagerManagementService)
         {
-            int count = mediaManagerManagementService.NumberOfPendingMedias();
-            return new JsonResult(count);
+            try
+            {
+                int count = mediaManagerManagementService.NumberOfPendingMedias();
+                return new JsonResult(count);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation("NumberOfPendingMedias: " + ex.Message);
+                return new JsonResult("Error occur");
+            }
         }
+
         public class Request
         {
             public int NumberOfPage { get; set; }
@@ -63,8 +109,16 @@ namespace Nextflip.APIControllers
         public JsonResult GetPendingMediasListAccordingRequest([FromServices] IMediaManagerManagementService mediaManagerManagementService,
                                 [FromBody] Request request)
         {
-            IEnumerable<MediaEditRequest> requests = mediaManagerManagementService.GetPendingMediasListAccordingRequest(request.NumberOfPage, request.RowsOnPage, request.RequestPage);
-            return new JsonResult(requests);
+            try
+            {
+                IEnumerable<MediaEditRequest> requests = mediaManagerManagementService.GetPendingMediasListAccordingRequest(request.NumberOfPage, request.RowsOnPage, request.RequestPage);
+                return new JsonResult(requests);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation("GetPendingMediasListAccordingRequest: " + ex.Message);
+                return new JsonResult("Error occur");
+            }
         }
     }
 }

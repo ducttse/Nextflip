c20a8ddc76cfee8d538378e8be6fde633e0f02a8
Long <tranbaolong14@gmail.com>
2021-06-17 22:25:42 +0700

    fix pagination

--- Nextflip/wwwroot/js/Supporter-Dashboard.js
+++ Nextflip/wwwroot/js/Supporter-Dashboard.js
@@ -2,7 +2,6 @@
   data: []
 };
 let requestParam = {
-  NumberOfPage: 5,
   RowsOnPage: 10,
   RequestPage: 1,
   TopicName: "Account"
@@ -41,66 +40,6 @@ function appendTicketToWrapper(start, end) {
   dataWapper.insertAdjacentHTML("afterbegin", ticketArray);
 }
 
-function onLoad(rowsPerPage) {
-  appendTicketToWrapper(0, rowsPerPage);
-  // appendPagination(Data.data.length, rowsPerPage);
-  // setCurrentColor(1);
-  // setClickToIndex(appendTicketToWrapper);
-}
-
-function setMaxPage(resolve) {
-  fetch("/api/ViewSupporterDashboard/GetNumOfSupportTicket")
-    .then((res) => res.json())
-    .then((json) => {
-      rowsPerPage = requestParam.RowsOnPage;
-      maxPage = parseInt(parseInt(json) / rowsPerPage);
-      resolve("resolved");
-    });
-}
-
-function CountCurrentLoadedPage() {
-  return parseInt(Data.data.length / requestParam.RowsOnPage)
-}
-
-function RequestMoreData(RequestPage) {
-  requestParam.RequestPage = RequestPage;
-  console.log(Data.data.length);
-
-  PostRequest(RequestPage).then(res => res.json()).then(json => {
-    json.forEach(user => {
-      Data.data.push(user);
-    })
-    let num = RequestPage - 1;
-    appendTicketToWrapper(num * rowsPerPage, (num + 1) * rowsPerPage);
-    setCurrentColor(RequestPage);
-  })
-}
-
-function preLoad() {
-  return new Promise((resolve, reject) => {
-    setMaxPage(resolve);
-  });
-}
-
-function PostRequest(page) {
-  let reqHeader = new Headers();
-  reqHeader.append("Content-Type", "text/json");
-  reqHeader.append("Accept", "application/json, text/plain, */*");
-  if (page !== 1) {
-    requestParam.RequestPage = page;
-  }
-  let initObject = {
-    method: "POST",
-    headers: reqHeader,
-    body: JSON.stringify(requestParam)
-  };
-  //
-  return fetch(
-    "/api/ViewSupporterDashboard/GetPendingSupportTickets",
-    initObject
-  )
-}
-
 function requestTopicData(topic) {
   requestParam.TopicName = topic;
   let reqHeader = new Headers();
@@ -119,7 +58,7 @@ function getTopics() {
     .then(res => res.json())
     .then(json => {
       TopicArr = json;
-      appendCollase(requestTopicData, appendTicketToWrapper);
+      appendCollase("Topic", requestTopicData, appendTicketToWrapper);
     })
 }
 

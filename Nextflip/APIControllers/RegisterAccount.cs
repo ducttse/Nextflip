7dc2da3e1661c2826fe42b094d374e50f4fd7187
longpnh21 <longpnh2001@gmail.com>
2021-06-26 17:12:44 +0700

    Add register + login

---
+++ Nextflip/APIControllers/RegisterAccount.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Nextflip.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nextflip.APIControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegisterAccount : ControllerBase
+    {
+        private readonly ILogger _logger;
+        public RegisterAccount(ILogger<RegisterAccount> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [Route("Register")]
+        [HttpPost]
+        public IActionResult Register([FromServices] IAccountService accountService, [FromBody]AccountRegisterForm form)
+        {
+            try
+            {
+                if (accountService.IsExistedEmail(form.UserEmail)) return new JsonResult(new { Message = "Email has already existed exist!" });
+                string result = accountService.RegisterAccount(form.UserEmail, form.GoogleID, form.GoogleEmail, form.Password, form.Fullname, form.DateOfBirth);
+                if (result == null) return new JsonResult(new { Message = "An Error Occurred" });
+                return new JsonResult(new { Message = result });
+            }
+            catch(Exception ex)
+            {
+                _logger.LogInformation("RegisterAccount/Register: " + ex.Message);
+                return new JsonResult(new { Message = ex.Message });
+            }
+        }
+    }
+    public partial class AccountRegisterForm
+    {
+        public string UserEmail { get; set; }
+        public string GoogleID { get; set; }
+        public string GoogleEmail { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string Fullname { get; set; }
+        public string DateOfBirth { get; set; }
+    }
+}
---
+++ Nextflip/Services/Implementations/AccountService.cs
@@ -0,0 +1,27 @@
+﻿using Nextflip.Models.account;
+using Nextflip.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nextflip.Services.Implementations
+{
+    public class AccountService : IAccountService
+    {
+        private readonly IAccountDAO _accountDAO;
+
+        public AccountService(IAccountDAO accountDAO)
+        {
+            _accountDAO = accountDAO;
+        }
+        public bool ChangeProfile(string userID, string userEmail, string password, string fullname, string dateOfBirth, string pictureURL) => _accountDAO.ChangeProfile(userID, userEmail, password, fullname, dateOfBirth, pictureURL);
+
+        public Account GetAccountByID(string userID) => _accountDAO.GetAccountByID(userID);
+
+        public bool Login(string email, string password) => _accountDAO.Login(email, password);
+        public bool IsExistedEmail(string email) => _accountDAO.IsExistedEmail(email);
+
+        public string RegisterAccount(string userEmail, string googleID, string googleEmail, string password, string fullname, string dateOfBirth) => _accountDAO.RegisterAnAccount(userEmail, googleID, googleEmail, password, fullname, dateOfBirth);
+    }
+}

48ed2b1ad28c2c44b30669433b7f6143a197a6a0
Long <tranbaolong14@gmail.com>
2021-07-12 18:52:05 +0700

    fix  some issue

--- Nextflip/wwwroot/js/Add-New-Media.js
+++ Nextflip/wwwroot/js/Add-New-Media.js
@@ -1,10 +1,1 @@
-﻿let modaAddlForm;
-
-function showForm() {
-    if (modaAddlForm == null) {
-        modaAddlForm = new bootstrap.Modal(document.getElementById('modalAddForm'), {
-            keyboard: false
-        })
-    }
-    modaAddlForm.show();
-}
+﻿

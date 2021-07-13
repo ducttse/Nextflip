3eedda1f4b5e62a6fc7395eb76d07fb18d1ab1b9
datlt2001 <thanhdat3001@gmail.com>
2021-07-13 15:07:03 +0700

    fix merge

--- Nextflip/Models/mediaCategory/IMediaCategoryDAO.cs
+++ Nextflip/Models/mediaCategory/IMediaCategoryDAO.cs
@@ -8,7 +8,6 @@ namespace Nextflip.Models.mediaCategory
     public interface IMediaCategoryDAO
     {
         IList<int> GetCategoryIDs(string mediaID);
-        IList<string> GetMediaIDs(int categoryID);
         bool AddMediaCategory(string mediaID, int categoryID);
         IList<string> GetMediaIDs(int categoryID, int limit);
     }

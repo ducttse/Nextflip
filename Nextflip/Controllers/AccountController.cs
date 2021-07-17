f5ed9996b65d6dc8fd0201bf7e75a32a04ad1041
longpnh21 <longpnh2001@gmail.com>
2021-07-17 11:33:38 +0700

    Merge LoginController and Register controller into AccountController

---
+++ Nextflip/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nextflip.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login() => View();
+
+        public IActionResult Register() => View();
+
+        public async Task<IActionResult> Logout()
+        {
+            HttpContext.Session.Clear();
+            HomeController homeController = new HomeController();
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return RedirectToAction("Index", homeController);
+        }
+
+
+    }
+}

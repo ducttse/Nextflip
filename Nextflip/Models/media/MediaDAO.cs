352d91d42c57258b6e885d0382d81a185c548673
ducthethird <ducttse@gmail.com>
2021-07-27 08:38:09 +0700

    trytodosth

--- Nextflip/Models/media/MediaDAO.cs
+++ Nextflip/Models/media/MediaDAO.cs
@@ -646,7 +646,7 @@ namespace Nextflip.Models.media
             {
                 using (var connection = new MySqlConnection(DbUtil.ConnectionString))
                 {
-
+                    connection.Open();
                     using MySqlCommand command = new MySqlCommand();
                     command.Connection = connection;
                     command.CommandType = CommandType.StoredProcedure;
@@ -673,7 +673,7 @@ namespace Nextflip.Models.media
                 var mediaForm = GetDetailedMedia(ID);
                 if (mediaForm.MediaInfo.Title != null && mediaForm.MediaInfo.Title.EndsWith("_preview"))
                 {
-                    var trueTitle = mediaForm.MediaInfo.Title.Remove(mediaForm.MediaInfo.Title.LastIndexOfAny("_preview".ToCharArray()), 8);
+                    var trueTitle = mediaForm.MediaInfo.Title.Remove(mediaForm.MediaInfo.Title.LastIndexOf("_preview"), 8);
                     var oldMediaID = GetMediasByTitle(trueTitle);
                     if (oldMediaID != null)
                     {
@@ -682,6 +682,7 @@ namespace Nextflip.Models.media
                         EditMedia(mediaForm);
                         ApproveChangeMedia(mediaForm.MediaInfo.MediaID);
                         RemoveMedia(ID);
+                        result = true;
                     }
                 }
                 else
@@ -1380,7 +1381,7 @@ namespace Nextflip.Models.media
                     var oldSeason =
                         oldMedia.Seasons.FirstOrDefault(ss => ss.SeasonInfo.SeasonID == newSeasonForm.SeasonInfo.SeasonID);
                     ///check if not changed
-                    if (newSeasonForm.SeasonInfo.EqualWithoutStatus(oldSeason.SeasonInfo)) {
+                    if (oldSeason != null && newSeasonForm.SeasonInfo.EqualWithoutStatus(oldSeason.SeasonInfo)) {
                         if (newSeasonForm.Episodes.Count() == oldSeason.Episodes.Count())
                         {
                             bool flag = false;
@@ -1454,6 +1455,7 @@ namespace Nextflip.Models.media
                 command.Parameters.AddWithValue("@bannerURL_Input", mediaInfo.BannerURL);
                 command.Parameters.AddWithValue("@language_Input", mediaInfo.Language);
                 command.Parameters.AddWithValue("@description_Input", mediaInfo.Description);
+                command.Parameters.AddWithValue("@status_Input", mediaInfo.Status);
                 result = command.ExecuteNonQuery();
             }
             return result;
